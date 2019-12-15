5cd3bab482c227a802a4497a8775394e5ca1cc6a
Lucas de Ávila Martins <lucasdeavilamartins@Mac-mini-de-Lucas.local>
2019-12-14 23:28:20 -0300

    wip

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,36 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+// const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+// Shell
+// filterUrls.get({ qs:{ id:"example-id" } })
+export const filterUrls = functions.https.onRequest(
+  async (request, response) => {
+    const id = request.query.id;
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+    if (id == null) {
+      response.json({ error: "Invalid filter id" });
+    } else {
+      const db = admin.firestore();
+      const res = await db
+        .collection("filters")
+        .where("shortId", "==", id)
+        .get();
+      let foundValue = false;
+      res.forEach(doc => {
+        if (foundValue) {
+          return;
+        }
+        foundValue = true;
+        const data = doc.data();
+        const image = data.imageUrl;
+        const lut = data.lutUrl;
+        response.json({ image, lut });
+      });
+
+      if (!foundValue) {
+        response.json({ error: "Filter not found by id" });
+      }
+    }
+  },
+);
--- gatsby-site/static/view/demo.js
+++ gatsby-site/static/view/demo.js
@@ -5,99 +5,152 @@ let THREECAMERA;
 
 // callback : launched if a face is detected or lost. TODO : add a cool particle effect WoW !
 function detect_callback(isDetected) {
-    if (isDetected) {
-        console.log("INFO in detect_callback() : DETECTED");
-    } else {
-        console.log("INFO in detect_callback() : LOST");
-    }
+  if (isDetected) {
+    console.log("INFO in detect_callback() : DETECTED");
+  } else {
+    console.log("INFO in detect_callback() : LOST");
+  }
 }
 
 // build the 3D. called once when Jeeliz Face Filter is OK
 function init_threeScene(spec, images) {
-    const threeStuffs = THREE.JeelizHelper.init(spec, detect_callback);
-    const mat = new THREE.MeshBasicMaterial({
-        map: new THREE.TextureLoader().load(images.top),
-        transparent: true,
-    });
-    const CLOUDMESH = new THREE.Mesh(new THREE.PlaneGeometry(3.75, 3.75), mat);
-    CLOUDMESH.position.setY(0.325);
-    CLOUDMESH.frustumCulled = false;
-    CLOUDMESH.renderOrder = 10000;
+  const threeStuffs = THREE.JeelizHelper.init(spec, detect_callback);
+  const mat = new THREE.MeshBasicMaterial({
+    map: new THREE.TextureLoader().load(images.top),
+    transparent: true,
+  });
+  const CLOUDMESH = new THREE.Mesh(new THREE.PlaneGeometry(3.75, 3.75), mat);
+  CLOUDMESH.position.setY(0.325);
+  CLOUDMESH.frustumCulled = false;
+  CLOUDMESH.renderOrder = 10000;
 
-    const CLOUDOBJ3D = new THREE.Object3D();
-    CLOUDOBJ3D.add(CLOUDMESH);
+  const CLOUDOBJ3D = new THREE.Object3D();
+  CLOUDOBJ3D.add(CLOUDMESH);
 
-    threeStuffs.faceObject.add(CLOUDOBJ3D);
+  threeStuffs.faceObject.add(CLOUDOBJ3D);
 
-    // CREATE THE CAMERA
-    THREECAMERA = THREE.JeelizHelper.create_camera();
+  // CREATE THE CAMERA
+  THREECAMERA = THREE.JeelizHelper.create_camera();
 } // end init_threeScene()
 const defaultLut = "/luts/lut0.png"; // TODO: substituir pelo png no tamanho certo
 const defaultImage = "/luts/lut0.png"; // TODO: substituir por algo transparente
-const getLutUrl = () => {
-    if (window.parent && window.parent._lut) {
-        return window.parent._lut;
-    }
 
-    return defaultLut;
-};
-const getImageUrl = () => {
-    if (window.parent && window.parent._img) {
-        return window.parent._img;
-    }
+function parseURL(url) {
+  var parser = document.createElement("a"),
+    searchObject = {},
+    queries,
+    split,
+    i;
+  // Let the browser do the work
+  parser.href = url;
+  // Convert query string to object
+  queries = parser.search.replace(/^\?/, "").split("&");
+  for (i = 0; i < queries.length; i++) {
+    split = queries[i].split("=");
+    searchObject[split[0]] = split[1];
+  }
+  return {
+    protocol: parser.protocol,
+    host: parser.host,
+    hostname: parser.hostname,
+    port: parser.port,
+    pathname: parser.pathname,
+    search: parser.search,
+    searchObject: searchObject,
+    hash: parser.hash,
+  };
+}
 
-    return defaultImage;
+const getInfo = (cb) => {
+  if (window.parent && window.parent._lut && window.parent._img) {
+    return cb({
+      lut: {
+        url: window.parent._lut,
+        size: 16,
+      },
+      images: {
+        top: window.parent._img,
+      },
+    });
+  }
+
+  const parsed = parseURL(window.location.href);
+  const pathname = parsed.pathname;
+  const url =
+    "https://us-central1-filterme.cloudfunctions.net/filterUrls?id=" + pathname;
+  console.log({ pathname, url });
+
+  return fetch(url)
+    .then((res) => {
+      const data = res.json();
+      console.log({ data });
+      if (!data) {
+        cb(null);
+      } else if (data.error) {
+        cb(null);
+      } else {
+        cb({
+          lut: {
+            url: data.lut,
+            size: 16,
+          },
+          images: {
+            top: data.image,
+          },
+        });
+      }
+    })
+    .catch(() => cb(null));
 };
+
 //launched by body.onload() :
 function main() {
-    const info = {
-        lut: {
-            url: getLutUrl(),
-            size: 16,
-        },
-        images: {
-            top: getImageUrl(),
-        },
-    };
-    JeelizResizer.size_canvas({
+  getInfo((info) => {
+    if (info) {
+      JeelizResizer.size_canvas({
         isFullScreen: true,
         canvasId: "jeeFaceFilterCanvas",
         callback: function(isError, bestVideoSettings) {
-            init_faceFilter(bestVideoSettings, info);
+          init_faceFilter(bestVideoSettings, info);
         },
         onResize: function() {
-            THREE.JeelizHelper.update_camera(THREECAMERA);
+          THREE.JeelizHelper.update_camera(THREECAMERA);
         },
-    });
+      });
+    } else {
+      console.error("could not get image!!!!");
+      // mostra erro
+    }
+  });
 } //end main()
 
 function init_faceFilter(videoSettings, info) {
-    JEEFACEFILTERAPI.init({
-        videoSettings: {
-            //increase the default video resolution since we are in full screen
-            idealWidth: 1280, //ideal video width in pixels
-            idealHeight: 800, //ideal video height in pixels
-            maxWidth: 1920, //max video width in pixels
-            maxHeight: 1920, //max video height in pixels
-        },
-        followZRot: true,
-        canvasId: "jeeFaceFilterCanvas",
-        NNCpath: "./dist/", // root of NNC.json file
-        callbackReady: function(errCode, spec) {
-            if (errCode) {
-                console.log("AN ERROR HAPPENS. SORRY BRO :( . ERR =", errCode);
-                return;
-            }
+  JEEFACEFILTERAPI.init({
+    videoSettings: {
+      //increase the default video resolution since we are in full screen
+      idealWidth: 1280, //ideal video width in pixels
+      idealHeight: 800, //ideal video height in pixels
+      maxWidth: 1920, //max video width in pixels
+      maxHeight: 1920, //max video height in pixels
+    },
+    followZRot: true,
+    canvasId: "jeeFaceFilterCanvas",
+    NNCpath: "./dist/", // root of NNC.json file
+    callbackReady: function(errCode, spec) {
+      if (errCode) {
+        console.log("AN ERROR HAPPENS. SORRY BRO :( . ERR =", errCode);
+        return;
+      }
 
-            console.log("INFO : JEEFACEFILTERAPI IS READY");
-            init_threeScene(spec, info.images);
-            THREE.JeelizHelper.registerCamera(THREECAMERA, info.lut);
-        }, // end callbackReady()
+      console.log("INFO : JEEFACEFILTERAPI IS READY");
+      init_threeScene(spec, info.images);
+      THREE.JeelizHelper.registerCamera(THREECAMERA, info.lut);
+    }, // end callbackReady()
 
-        // called at each render iteration (drawing loop)
-        callbackTrack: function(detectState) {
-            TWEEN.update();
-            THREE.JeelizHelper.render(detectState, THREECAMERA);
-        }, // end callbackTrack()
-    }); // end JEEFACEFILTERAPI.init call
+    // called at each render iteration (drawing loop)
+    callbackTrack: function(detectState) {
+      TWEEN.update();
+      THREE.JeelizHelper.render(detectState, THREECAMERA);
+    }, // end callbackTrack()
+  }); // end JEEFACEFILTERAPI.init call
 } // end main()
